7adfa004358e9b1f7614c8acb7958b35988b9880
Jason Eng <99120605+jasonengs@users.noreply.github.com>
2025-03-03 20:14:06 +0800

    Update keys from snake case to camel case for JSON file

--- to_json.py
+++ to_json.py
@@ -1,6 +1,23 @@
 import json
 import pandas as pd
 
+
+def rename_columns(column):
+    results = []
+    for i in column:
+        split_column_name = i.split("_")
+
+        if len(split_column_name) > 1:
+            results.append(
+                split_column_name[0]
+                + "".join(word.capitalize() for word in split_column_name[1:])
+            )
+
+        else:
+            results.append(split_column_name[0])
+    return results
+
+
 df = pd.read_csv("./assets/data/latest_data.csv")
 
 # Drop some columns
@@ -47,37 +64,43 @@ df_final = df_merge.merge(df_roles_merge, how="inner", left_on="id", right_on="h
 # Remove hero id column from df_roles_merge
 df_final = df_final.drop(columns=["hero_id"])
 
+
+renamed_column = rename_columns(df_final.columns.to_list())
+
+for i, col in enumerate(df_final.columns):
+    df_final.columns.values[i] = renamed_column[i]
+
 # Save to JSON
 df_final.to_json("./assets/data/latest_data.json", orient="records")
 
 stats_key = [
-    "base_strength",
-    "base_agility",
-    "base_intelligence",
-    "strength_gain",
-    "agility_gain",
-    "intelligence_gain",
-    "base_health",
-    "base_mana",
-    "base_health_regeneration",
-    "base_mana_regeneration",
-    "base_armor",
-    "base_attack_speed",
-    "base_min_attack",
-    "base_max_attack",
-    "base_avg_attack",
-    "attack_range",
+    "baseStrength",
+    "baseAgility",
+    "baseIntelligence",
+    "strengthGain",
+    "agilityGain",
+    "intelligenceGain",
+    "baseHealth",
+    "baseMana",
+    "baseHealthRegeneration",
+    "baseManaRegeneration",
+    "baseArmor",
+    "baseAttackSpeed",
+    "baseMinAttack",
+    "baseMaxAttack",
+    "baseAvgAttack",
+    "attackRange",
     "bat",
-    "projectile_speed",
-    "attack_point",
-    "attack_backswing",
-    "movement_speed_daytime",
-    "turn_rate",
-    "vision_range_daytime",
-    "vision_range_nighttime",
+    "projectileSpeed",
+    "attackPoint",
+    "attackBackswing",
+    "movementSpeedDaytime",
+    "turnRate",
+    "visionRangeDaytime",
+    "visionRangeNighttime",
 ]
 
-images_key = ["hero_images_path", "hero_icons_path", "attribute_icons_path"]
+images_key = ["heroImagesPath", "heroIconsPath", "attributeIconsPath"]
 
 
 with open("./assets/data/latest_data.json", "r") as f:
